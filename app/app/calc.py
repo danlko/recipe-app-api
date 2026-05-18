90fa326702bcd676b406c2d57972a2d99b289c59
grecnhy <drachkev@gmail.com>
2026-05-18 11:11:59 +0300

    DB + simple tests

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions for testing purposes.
+"""
+
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers."""
+    return x - y
---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(10, 5)
+
+        self.assertEqual(res, 15)
+    
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, -5)
